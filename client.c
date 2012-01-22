413671fe7eef5f8ea75d741be840b8f3bf9ac10c
Jignesh Kakadiya <jigneshhk1992@gmail.com>
2012-01-22 17:39:49 +0530

    Some code cleanup

--- client.c
+++ client.c
@@ -18,105 +18,120 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.* 
  *
  */
-/* client process */ 
-#include <ctype.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
+/* client process */
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include<stdio.h>
 #include <string.h>
 #include <pthread.h>
-#define SIZE sizeof(struct sockaddr_in) 
-void firstconn();
-void* recieve();
-void* getmsg();
-void catcher(int sig); 
-int sockfd; 
+#define SIZE sizeof(struct sockaddr_in)
+void firstconn ();
+void *recieve ();
+void *getmsg ();
+void catcher (int sig);
+int sockfd;
 char msg[256];
 pthread_t thrd1;
 pthread_t thrd2;
 
-int main(void) 
-{ 
-
-struct sockaddr_in server = {AF_INET, 7001}; 
-/* convert and store the server's IP address */ 
-server.sin_addr.s_addr = inet_addr("10.100.93.58"); 
-
-/* set up the transport end point */ 
-if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
-{ 
-	perror("socket call failed"); 
-	exit(1); 
-} 
-/* connect the socket to the server's address */ 
-if ( connect (sockfd, (struct sockaddr *)&server, SIZE) == -1) 
-{ 
-	perror("connect call failed"); 
-	exit(1); 
-} 
-
-firstconn();
-/* send and receive information with the server */ 
-//pthread_create(&thrd1, NULL,getmsg ,NULL);	// handle data from a client
-//pthread_join(thrd1, NULL);
-recieve();
-pthread_create(&thrd2, NULL,getmsg ,NULL);	// handle data from a client
-pthread_join(thrd2, NULL);
-	
-}
-void* recieve()
+int
+main (void)
 {
- if( fork() == 0)
-  { int bytes;   
-	for(;;){
-	bytes = recv(sockfd,msg,sizeof(msg), 0);
-		
-	if (bytes > 0) 
-		{// printf("recieved..\n");
-		 printf("%s\n", msg); } 
-	
-	if(bytes == 0)
-           {printf("server is no more running");
-            close(sockfd);
-            exit(0);}
-      }
-  }
-}
-void* getmsg()
-{  
-  printf("------------------------MENU---------------------------\n\n");
-  printf("To view exiting members type  'list:'\n");
-  printf("To multicast type             'multicast:'\n");
-  printf("To send message to perticular nick type 'nick:message' \n\n");
-  printf("-------------------------------------------------------\n");
 
-  while(1)
-     { 
-	unsigned int i;int p;
-	a://printf("\nENTER nick:msg \n");
-	  gets(msg);
-          p=-1;
-          for(i = 0 ; i < strlen(msg) ; i++)
-             if( msg[i] == ':')
-               p = 1;
-          if(p == 1)
-             send(sockfd,msg,sizeof(msg),0);	 
-          else goto a;
-      }
+  struct sockaddr_in server = { AF_INET, 7001 };
+/* convert and store the server's IP address */
+  server.sin_addr.s_addr = inet_addr ("10.100.93.58");
+
+/* set up the transport end point */
+  if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+      perror ("socket call failed");
+      exit (1);
+    }
+/* connect the socket to the server's address */
+  if (connect (sockfd, (struct sockaddr *) &server, SIZE) == -1)
+    {
+      perror ("connect call failed");
+      exit (1);
+    }
+
+  firstconn ();
+/* send and receive information with the server */
+//pthread_create(&thrd1, NULL,getmsg ,NULL);    // handle data from a client
+//pthread_join(thrd1, NULL);
+  recieve ();
+  pthread_create (&thrd2, NULL, getmsg, NULL);	// handle data from a client
+  pthread_join (thrd2, NULL);
+
 }
 
-void firstconn()
-{    while(1)
-        {
-         printf("enter your nick : ");
-	 gets( msg ); 
-         if(strcmp(msg,""))
-          { send(sockfd,msg, sizeof(msg), 0); 
-	   break;}
-	 else continue;
-	} 
+void *
+recieve ()
+{
+  if (fork () == 0)
+    {
+      int bytes;
+      for (;;)
+	{
+	  bytes = recv (sockfd, msg, sizeof (msg), 0);
+
+	  if (bytes > 0)
+	    {			// printf("recieved..\n");
+	      printf ("%s\n", msg);
+	    }
+
+	  if (bytes == 0)
+	    {
+	      printf ("server is no more running");
+	      close (sockfd);
+	      exit (0);
+	    }
+	}
+    }
 }
 
+void *
+getmsg ()
+{
+  printf ("------------------------MENU---------------------------\n\n");
+  printf ("To view exiting members type  'list:'\n");
+  printf ("To multicast type             'multicast:'\n");
+  printf ("To send message to perticular nick type 'nick:message' \n\n");
+  printf ("-------------------------------------------------------\n");
 
+  while (1)
+    {
+      unsigned int i;
+      int p;
+    a:				//printf("\nENTER nick:msg \n");
+      gets (msg);
+      p = -1;
+      for (i = 0; i < strlen (msg); i++)
+	if (msg[i] == ':')
+	  p = 1;
+      if (p == 1)
+	send (sockfd, msg, sizeof (msg), 0);
+      else
+	goto a;
+    }
+}
+
+void
+firstconn ()
+{
+  while (1)
+    {
+      printf ("enter your nick : ");
+      gets (msg);
+      if (strcmp (msg, ""))
+	{
+	  send (sockfd, msg, sizeof (msg), 0);
+	  break;
+	}
+      else
+	continue;
+    }
+}
--- mserver.c
+++ mserver.c
@@ -19,247 +19,287 @@
  *
  */
 
-/* server process */ 
-#include <ctype.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <signal.h> 
+/* server process */
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 
-#define SIZE sizeof(struct sockaddr_in) 
+#define SIZE sizeof(struct sockaddr_in)
 #define MAX_THREADS 5
-void multicast();
-int searchnick();	
-void* handle_data(); 	
-void* handle_new_connection();
-void catcher(int sig); 
-void* firstconn();	
-int filedes; 
-fd_set master;    // master file descriptor list
+void multicast ();
+int searchnick ();
+void *handle_data ();
+void *handle_new_connection ();
+void catcher (int sig);
+void *firstconn ();
+int filedes;
+fd_set master;			// master file descriptor list
 fd_set read_fds;
-static char nick_array[10][256];	
-int mainsocketfd,max,i,j; 
-int bytes;	
+static char nick_array[10][256];
+int mainsocketfd, max, i, j;
+int bytes;
 char msg[256];
 pthread_t threads[MAX_THREADS];
 pthread_t thrd;
 static char *name[MAX_THREADS];
 
 /* main method starts here */
-int main(void) 
-	{ 
-	
-	struct sockaddr_in serverdetail = {AF_INET, 7001, INADDR_ANY}; 
-	static struct sigaction act; 
-	act.sa_handler = catcher; 
-	sigfillset(&(act.sa_mask)); 
-	sigaction(SIGPIPE, &act, NULL); 
+int
+main (void)
+{
 
-	FD_ZERO(&master);    // clear the master 
-    	FD_ZERO(&read_fds);
-	char *lst;
-        lst="list";
-        strcpy(nick_array[0],lst);
-	lst="multicast";
-        strcpy(nick_array[1],lst);
+  struct sockaddr_in serverdetail = { AF_INET, 7001, INADDR_ANY };
+  static struct sigaction act;
+  act.sa_handler = catcher;
+  sigfillset (&(act.sa_mask));
+  sigaction (SIGPIPE, &act, NULL);
 
-        /* set up the transport end point */ 
-	if ( (mainsocketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
-		{ 
-		perror("socket call failed"); 
-		exit(1); 
-		} 
+  FD_ZERO (&master);		// clear the master 
+  FD_ZERO (&read_fds);
+  char *lst;
+  lst = "list";
+  strcpy (nick_array[0], lst);
+  lst = "multicast";
+  strcpy (nick_array[1], lst);
 
-	
-	/* bind an address to the end point */ 
-	if ( bind(mainsocketfd, (struct sockaddr *)&serverdetail, SIZE) == -1) 
-		{ 
-		perror("bind call failed"); 
-		exit(1); 
-		} 
+  /* set up the transport end point */
+  if ((mainsocketfd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+      perror ("socket call failed");
+      exit (1);
+    }
 
-	printf("server is running ...have fun :p\n");
 
-	/* start listening for incoming connections */ 
-	if ( listen(mainsocketfd, 5) == -1 ) 
-	{ 
-		perror("listen call failed"); 
-		exit(1) ; 
-	} 
+  /* bind an address to the end point */
+  if (bind (mainsocketfd, (struct sockaddr *) &serverdetail, SIZE) == -1)
+    {
+      perror ("bind call failed");
+      exit (1);
+    }
 
-	FD_SET(mainsocketfd, &master);
-        max = mainsocketfd; 
-	      
-	while(1) 
-	{ 
-		read_fds = master; // copy it
-	        if (select(max+1, &read_fds, NULL, NULL, NULL) == -1) {
-		            perror("select");
-            		    exit(4);
-                }
-          
-	// run through the existing connections looking for data to read
-        for(i = mainsocketfd; i <= max; i++) {
-            if (FD_ISSET(i, &read_fds)) { 
-                if (i == mainsocketfd) {
-                   pthread_create(&threads[i], NULL, handle_new_connection,NULL);     // handle new connections
-                   pthread_join(threads[i], NULL); 
-		   } else {
-                       pthread_create(&threads[i], NULL, handle_data,NULL);	// handle data from a client
-		       pthread_join(threads[i], NULL);		       
-			} 
-            } 
-        } 
-   } 
-} 
+  printf ("server is running ...have fun :p\n");
+
+  /* start listening for incoming connections */
+  if (listen (mainsocketfd, 5) == -1)
+    {
+      perror ("listen call failed");
+      exit (1);
+    }
+
+  FD_SET (mainsocketfd, &master);
+  max = mainsocketfd;
+
+  while (1)
+    {
+      read_fds = master;	// copy it
+      if (select (max + 1, &read_fds, NULL, NULL, NULL) == -1)
+	{
+	  perror ("select");
+	  exit (4);
+	}
+
+      // run through the existing connections looking for data to read
+      for (i = mainsocketfd; i <= max; i++)
+	{
+	  if (FD_ISSET (i, &read_fds))
+	    {
+	      if (i == mainsocketfd)
+		{
+		  pthread_create (&threads[i], NULL, handle_new_connection, NULL);	// handle new connections
+		  pthread_join (threads[i], NULL);
+		}
+	      else
+		{
+		  pthread_create (&threads[i], NULL, handle_data, NULL);	// handle data from a client
+		  pthread_join (threads[i], NULL);
+		}
+	    }
+	}
+    }
+}
 
 /*it will handles the new connection and give new socket to the client to communicate*/
 
-void *handle_new_connection()
+void *
+handle_new_connection ()
 {
-filedes = accept(mainsocketfd,NULL,NULL);
+  filedes = accept (mainsocketfd, NULL, NULL);
 
-                    if (filedes == -1) {
-                        perror("accept");
-                    } else {
-                        FD_SET(filedes, &master); // add to master set
-                        if (filedes > max) {    // keep track of the max
-                            max = filedes;
-                        }
-                        pthread_create(&thrd, NULL, firstconn ,NULL);	
-		            pthread_join(thrd, NULL);	
-                        
-                    }
+  if (filedes == -1)
+    {
+      perror ("accept");
+    }
+  else
+    {
+      FD_SET (filedes, &master);	// add to master set
+      if (filedes > max)
+	{			// keep track of the max
+	  max = filedes;
+	}
+      pthread_create (&thrd, NULL, firstconn, NULL);
+      pthread_join (thrd, NULL);
+
+    }
 
 }
 
 /* this method will handles the data send by client and according to the nick it will
    send to that client.*/
 
-void *handle_data()
-{  	
-  bytes=recv(i,msg, sizeof(msg), 0);       
-     
-	//send(i,"server::msg recieved...\n",256,0); 
-if(bytes==0) {
-	if (bytes == 0) {
-		char *p; p="";
-		printf("socket %d hung up with nick : %s\n", i,nick_array[i]);
-		strcpy(nick_array[i],p);}
-                else 
-			perror("recv");
-
-         close(i); 
-         FD_CLR(i, &master); }
-	 else {
-		char *ptr; 
-		int nk;
-		char niktemp[10],temp[300];
-                int t,w;
-		int y;			
-		char msgtemp[256];
-                ptr= strchr(msg,':');      // returns the pointer to :
-                nk = ptr - msg;	
-                //============================seperating nick from msg 			
-		for(t = 0 ; t < nk ; t++)
-                       	niktemp[t] = msg[t];
-                niktemp[t] = '\0';  
-                //=====================================================
-		//===================seperating actual message form msg			
-		for(t = 0 ; t < strlen(nick_array[i]) ; ++t)
-                       	msgtemp[t] = nick_array[i][t];
-		msgtemp[t++] = '-';
-                msgtemp[t++] = '>';			
-		
-		for(w = nk+1,y = t; msg[w] != '\0' ; y++, w++)
-                       	msgtemp[y] = msg[w];  
-		msgtemp[y] = '\0';
-		//printf("%s \n",msgtemp);			
-		//=====================================================
-		printf("==========================================\n");			
-		                        
-		if (FD_ISSET(i, &master)) {
-		    if(searchnick(niktemp) > mainsocketfd) {  
-			//ptr = strcat(temp,ptr);
-		        printf("sending to socket %d\n",searchnick(niktemp)); 
-                       	if(send(searchnick(niktemp),msgtemp,sizeof(msg),0)==-1)
-                            perror("send");
-		      	    printf("sending complete\n"); }
-                       	    else if(searchnick(niktemp)==0) {  
-				int ls; 
-				char *abc;
-				printf("sending list to client at %d \n",i);
-				for(ls=mainsocketfd;ls<=max;++ls)
-	                            send(i,nick_array[ls],sizeof(msg),0);}
-                       		    else if(searchnick(niktemp)==1) { 
-					printf("multicast request by client at %d\n",i);
-                               		multicast(); }
-		                        else { 
-					    send(i,"server::nick name is currently not in the list .. :( \n",sizeof(msg),0);
-                               		    printf("::: delivering failed no such nick exists :::\n");}
-                         
-				            printf("==========================================\n");  }
-                                 
-	}
-   
-}
-
-void multicast()
+void *
+handle_data ()
 {
- for(j = mainsocketfd+1 ; j <= max ; j++)
-     { 
-       if(j != mainsocketfd && i != j)
-          {
-            printf("%d sending to socket %d th \n",i,j);         
-            //send(j,nick_array[j],sizeof(nick_array),0);				   
-	   if (send(j, msg, bytes, 0) == -1) {
-                perror("send ");
-          }
-           
-	} 
-     }
+  bytes = recv (i, msg, sizeof (msg), 0);
+
+  //send(i,"server::msg recieved...\n",256,0); 
+  if (bytes == 0)
+    {
+      if (bytes == 0)
+	{
+	  char *p;
+	  p = "";
+	  printf ("socket %d hung up with nick : %s\n", i, nick_array[i]);
+	  strcpy (nick_array[i], p);
+	}
+      else
+	perror ("recv");
+
+      close (i);
+      FD_CLR (i, &master);
+    }
+  else
+    {
+      char *ptr;
+      int nk;
+      char niktemp[10], temp[300];
+      int t, w;
+      int y;
+      char msgtemp[256];
+      ptr = strchr (msg, ':');	// returns the pointer to :
+      nk = ptr - msg;
+      //============================seperating nick from msg                  
+      for (t = 0; t < nk; t++)
+	niktemp[t] = msg[t];
+      niktemp[t] = '\0';
+      //=====================================================
+      //===================seperating actual message form msg                 
+      for (t = 0; t < strlen (nick_array[i]); ++t)
+	msgtemp[t] = nick_array[i][t];
+      msgtemp[t++] = '-';
+      msgtemp[t++] = '>';
+
+      for (w = nk + 1, y = t; msg[w] != '\0'; y++, w++)
+	msgtemp[y] = msg[w];
+      msgtemp[y] = '\0';
+      //printf("%s \n",msgtemp);                      
+      //=====================================================
+      printf ("==========================================\n");
+
+      if (FD_ISSET (i, &master))
+	{
+	  if (searchnick (niktemp) > mainsocketfd)
+	    {
+	      //ptr = strcat(temp,ptr);
+	      printf ("sending to socket %d\n", searchnick (niktemp));
+	      if (send (searchnick (niktemp), msgtemp, sizeof (msg), 0) == -1)
+		perror ("send");
+	      printf ("sending complete\n");
+	    }
+	  else if (searchnick (niktemp) == 0)
+	    {
+	      int ls;
+	      char *abc;
+	      printf ("sending list to client at %d \n", i);
+	      for (ls = mainsocketfd; ls <= max; ++ls)
+		send (i, nick_array[ls], sizeof (msg), 0);
+	    }
+	  else if (searchnick (niktemp) == 1)
+	    {
+	      printf ("multicast request by client at %d\n", i);
+	      multicast ();
+	    }
+	  else
+	    {
+	      send (i,
+		    "server::nick name is currently not in the list .. :( \n",
+		    sizeof (msg), 0);
+	      printf ("::: delivering failed no such nick exists :::\n");
+	    }
+
+	  printf ("==========================================\n");
+	}
+
+    }
+
+}
+
+void
+multicast ()
+{
+  for (j = mainsocketfd + 1; j <= max; j++)
+    {
+      if (j != mainsocketfd && i != j)
+	{
+	  printf ("%d sending to socket %d th \n", i, j);
+	  //send(j,nick_array[j],sizeof(nick_array),0);                                  
+	  if (send (j, msg, bytes, 0) == -1)
+	    {
+	      perror ("send ");
+	    }
+
+	}
+    }
 }
 
 
-int searchnick(char *nik)
-{ int k;
-  
-  for( k = 0 ; k <= max ; ++k )
-  	if( !strcmp(nick_array[k],nik) )
-  	      return k;
+int
+searchnick (char *nik)
+{
+  int k;
+
+  for (k = 0; k <= max; ++k)
+    if (!strcmp (nick_array[k], nik))
+      return k;
   return -1;
 }
 
-void *firstconn()
-{	
-  char nick[256];int n=0;
-  bytes=recv(filedes,msg, sizeof(msg), 0); 
-  while(1) {
-	if(bytes > 0) {
-	      strcpy(nick,msg);
-	      printf("=============================================\n");
-              printf("new connection at %d \n",filedes);
-	      printf("nick : %s \n",nick);
-              strcpy(nick_array[filedes],msg);
-	      printf("SYN_ACK send...\n");
-	      send(filedes,"server::connected to server .....\n",256,0);
-              printf("\ncurrent existing members.....\n");			
-	      for(n=mainsocketfd;n<=filedes;n++)
-                    printf("%s \n",nick_array[n]);
-             
-              printf("=============================================\n");
-	      break;
-	     } }
+void *
+firstconn ()
+{
+  char nick[256];
+  int n = 0;
+  bytes = recv (filedes, msg, sizeof (msg), 0);
+  while (1)
+    {
+      if (bytes > 0)
+	{
+	  strcpy (nick, msg);
+	  printf ("=============================================\n");
+	  printf ("new connection at %d \n", filedes);
+	  printf ("nick : %s \n", nick);
+	  strcpy (nick_array[filedes], msg);
+	  printf ("SYN_ACK send...\n");
+	  send (filedes, "server::connected to server .....\n", 256, 0);
+	  printf ("\ncurrent existing members.....\n");
+	  for (n = mainsocketfd; n <= filedes; n++)
+	    printf ("%s \n", nick_array[n]);
+
+	  printf ("=============================================\n");
+	  break;
+	}
+    }
 }
 
 
-void catcher(int sig) 
-{ 
-	close(filedes); 
-	exit (0); 
+void
+catcher (int sig)
+{
+  close (filedes);
+  exit (0);
 }
-
